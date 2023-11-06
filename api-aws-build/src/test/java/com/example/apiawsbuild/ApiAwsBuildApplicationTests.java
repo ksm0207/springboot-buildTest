ad144c9f15b761920e77392db2a7913b6327b7d7
Kimsungmin <ksm03071@naver.com>
2023-11-06 17:54:03 +0900

    re-init

--- api-aws-build/src/test/java/com/example/apiawsbuild/ApiAwsBuildApplicationTests.java
+++ api-aws-build/src/test/java/com/example/apiawsbuild/ApiAwsBuildApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.example;
+package com.example.apiawsbuild;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExampleApplicationTests {
+class ApiAwsBuildApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
